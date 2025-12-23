8000792a7572fef528d84ddb4cd284d4726fb214
Gerald Patrick <clear3sixty@gmail.com>
2025-12-23 17:09:28 +0300

    Initial setup

---
+++ src/utils/navigation.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+  { href: '/', text: 'Home' },
+  { href: '/about', text: 'About' },
+  { href: '/academics', text: 'Academics' },
+  { href: '/admissions', text: 'Admissions' },
+  { href: '/contact', text: 'Contact' },
+];
